6de86360d36d6bcdefa2f724691482af8a868600
Codewithimad <imadhussain6655@gmail.com>
2025-08-17 19:57:18 +0300

    Updated

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,22 @@ const allowedOrigins = [
 
 
 
-app.use(cors({
-    origin: allowedOrigins,   // just pass the array
-    credentials: true
-}));
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 
 
 

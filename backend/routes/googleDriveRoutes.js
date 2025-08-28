924e59a0e7358a772b9f8dc96a1fde192758579c
Codewithimaad <imadhussain6655@gmail.com>
2025-08-29 00:47:33 +0300

    Added my project files

--- backend/routes/googleDriveRoutes.js
+++ backend/routes/googleDriveRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { grantDriveAccess, revokeDriveAccess } from "../controllers/googleDriveController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // POST /api/drive/grant
-router.post("/grant", grantDriveAccess);
+router.post("/grant",protect, grantDriveAccess);
 
 // POST /api/drive/revoke
-router.post("/revoke", revokeDriveAccess);
+router.post("/revoke",protect, revokeDriveAccess);
 
 export default router;
